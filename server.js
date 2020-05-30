9cdc1fa9479c6ca965269c6096f8fd2096cb3694
Bruno Paula <brunopaula@dryserv.com>
2020-05-30 14:41:29 -0400

    added a get route

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const app = express()
 
 
 
+app.get('/', (req,res) => {
+    res.send('<h1>Hello Bruno</h1>')
+})
+
+
 const port = process.env.PORT || 3000
 
 app.listen(port, () => console.log(`Server Running on port ${port}`))
