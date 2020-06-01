ca1a816c3c955d3d5605488fb62834a1bca6cdf7
Bruno Paula <brunopaula@dryserv.com>
2020-06-01 13:40:38 -0400

    Addred Crud
    added all crud endpoints

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,27 +7,85 @@ const User = require('../../models/user')
 //@route GET api/users
 //@desc  Get all Users
 //@access Public
-router.get('/', (req,res) => {
-    User.find()
-        .sort({ date: -1 })
-        .then( users => res.json(users) )
+router.get('/',  async (req,res) => {
+   
+    try {
+       const result = await User.find().sort({ date: -1 })
+       res.send(result)
+          
+     } catch (error) {
+        res.json(error)
+     }
 })
 
+//@route GET api/users/id
+//@desc  Get one user by Id
+//@access Public
+router.get('/:id', async (req,res) => {
+    
+   try {
+    const id = req.params.id
+    const result = await User.findById(id)
+    res.json(result)
+   } catch (error) {
+    res.json({error:error})
+   }
+        
+})
 
 //@route POST api/users
 //@desc  Create 1 User
 //@access Public
-router.post('/', (req,res) => {
-   const newUser = new User({
+
+router.post('/', async(req,res) => {
+
+    const newUser = await new User({
         first: req.body.first,
         last: req.body.last
     })
-    newUser.save()
-        .then( userData => res.json(userData))
-        .catch( err => console.log(err))
+    try {
+       const result = await newUser.save()
+       res.json({sucess:'Created', result:result})
+    } catch (error) {
+        res.json(error)
+    }
+   
+ })
+
+//@route DELETE api/users/:id
+//@desc  Delete User by id
+//@access Public
+router.delete('/:id', async(req,res) => {
+  
+    try {
+    const id = req.params.id
+    const result = await User.findByIdAndDelete(id)
+        res.json({sucess: `Deleted ${id}`, result: result})
+    
+   } catch (error) {
+    res.json({failed: "Invalid Id or User"})
+ }
+
 })
 
+//@route UPDADE api/users/:id
+//@desc  Updade User by Id
+//@access Public
+router.patch('/:id', async (req,res) => {
+        
+      try {
+        const id = req.params.id
+        const update = req.body  
+        const options = {new: true}
+        const result = await User.findByIdAndUpdate(id,update,options)
+         res.send(result)
+      } catch (error) {
+        res.json({failed:error})
+      }
+        
 
+    
+ })
 
 
 module.exports = router
